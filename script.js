11c67a7aba2c05b7a01005285680a7add4f91a35
Mauro Marcosano <mauromarcosano@gmail.com>
2024-05-14 19:20:05 -0300

    First game working well

--- script.js
+++ script.js
@@ -14,9 +14,18 @@ const Gameboard = (() => {
     }
   };
 
+  //Method to reset the board
+  const resetGameboard = () => {
+    //Change all array elements to ''
+    for (let i = 0; i < gameboard.length; i++) {
+      gameboard[i] = '';
+    }
+  };
+
   return {
     gameboard,
     updateGameboard,
+    resetGameboard,
   };
 })();
 
@@ -31,30 +40,108 @@ const Player = (symbol) => {
 const playerX = Player('X');
 const playerO = Player('O');
 
-console.log(playerX);
-console.log(playerO);
-
 //IIFE for the game
 const game = (() => {
   //First player is always X
   let nowPlaying = playerX;
-  //Player change
-  let squareSelect = prompt('Choose a square (0-8): ');
+  //Game active true until a winner or a draw
+  let gameActive = true;
 
-  //First game
-  if (Gameboard.updateGameboard(squareSelect, nowPlaying.symbol)) {
-    console.log('You chose: ' + squareSelect);
-    console.log('Updated gameboard:', Gameboard.gameboard);
-  } else {
-    console.log('Square already occupied. Try again');
-  }
+  //Principal PUBLIC game METHOD
+  const playTurn = () => {
+    while (gameActive) {
+      console.log(nowPlaying);
+      let squareSelect = prompt('Choose a square (0-8): ');
+      squareSelect = parseInt(squareSelect);
 
-  //Second game ... not coded yet
-  nowPlaying === playerX ? (nowPlaying = playerO) : (nowPlaying = playerX);
+      if (!isNaN(squareSelect) && squareSelect >= 0 && squareSelect <= 8) {
+        //First game
 
-  console.log(nowPlaying);
-  return {
-    nowPlaying,
+        if (Gameboard.updateGameboard(squareSelect, nowPlaying.symbol)) {
+          console.log('You chose: ' + squareSelect);
+          console.log('Updated gameboard:', Gameboard.gameboard);
+          //Verify game conditions
+          if (checkWinner() || checkFullBoard()) {
+            if (resetGame()) {
+            } else {
+              gameActive = false;
+              break;
+            }
+          }
+
+          //Player change
+          nowPlaying === playerX
+            ? (nowPlaying = playerO)
+            : (nowPlaying = playerX);
+        } else {
+          console.log('Square already occupied. Try again');
+        }
+      } else {
+        console.log('Invalid input');
+      }
+    }
   };
-  1;
+
+  //Function to check if there is any winner
+  const checkWinner = () => {
+    const winningConditions = [
+      //Rows
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      //Cross
+      [0, 4, 8],
+      [2, 4, 6],
+      //Columns
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+    ];
+    for (let conditions of winningConditions) {
+      const [a, b, c] = conditions;
+      if (
+        Gameboard.gameboard[a] !== '' &&
+        Gameboard.gameboard[a] === Gameboard.gameboard[b] &&
+        Gameboard.gameboard[a] === Gameboard.gameboard[c]
+      ) {
+        alert(`Congratulatios! ${nowPlaying.symbol} wins!`);
+        return true; //There´s a winner
+      }
+    }
+
+    return false;
+  };
+
+  //Function to check if the board is full
+  const checkFullBoard = () => {
+    let isFull = false;
+    isFull = Gameboard.gameboard.every((element) => element !== '');
+    if (isFull) {
+      alert('Its a draw! :(');
+    }
+    return isFull;
+  };
+
+  const resetGame = () => {
+    let answer;
+    do {
+      answer = prompt('Do you want to play again? (yes / no)');
+      answer = answer ? answer.toLowerCase() : '';
+    } while (answer !== 'yes' && answer !== 'no');
+    if (answer === 'yes') {
+      Gameboard.resetGameboard();
+      nowPlaying = playerX;
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  //Returning public method (function to play a turn)
+  return {
+    playTurn,
+  };
 })();
+
+//Execute the game
+game.playTurn();
